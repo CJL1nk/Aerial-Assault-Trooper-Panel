2f5c76bd799e2b7fbd21beb711a4c883d52d50a0
CJ_Link <99633691+CJL1nk@users.noreply.github.com>
2026-01-03 22:47:33 -0800

    got working

---
+++ README.md
@@ -0,0 +1,5 @@
+THIS DOES NOT WORK!!!!
+
+THIS IS SIMPLY A RECREATION OF A REALLY FUNNY JOKE
+
+This does NOTHING and serves no purpose other than humor because the original gif it came from was the funniest thing I've seen in a long time
---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <QApplication>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QPixmap>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Aerial Assault Trooper DDOS Panel");
+    window.resize(800, 500);
+
+    // Background troop[er
+    window.setStyleSheet(
+        "QWidget {"
+        "background-image: url(./resources/background.jpg);"
+        "background-repeat: no-repeat;"
+        "background-position: center;"
+        "}"
+    );
+
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+    layout->setContentsMargins(20, 20, 20, 20);
+    layout->setSpacing(15);
+
+    // Big banner
+    QLabel *banner = new QLabel;
+    QPixmap bannerImg("./resources/banner.png");
+    banner->setPixmap(bannerImg.scaledToHeight(50, Qt::SmoothTransformation));
+    banner->setAlignment(Qt::AlignCenter);
+    banner->setStyleSheet("background: transparent;");
+
+    // IP Input
+    QLineEdit *ipInput = new QLineEdit;
+    ipInput->setPlaceholderText("Enter IP");
+    ipInput->setStyleSheet("background: white; color: black;");
+    ipInput->setMaximumWidth(300);
+
+    // Port Input
+    QLineEdit *portInput = new QLineEdit;
+    portInput->setPlaceholderText("Enter Port");
+    portInput->setStyleSheet("background: white; color: black;");
+    portInput->setMaximumWidth(300);
+
+    // Put on same row
+    QHBoxLayout *inputRow = new QHBoxLayout;
+    inputRow->addStretch();
+    inputRow->addWidget(ipInput);
+    inputRow->addSpacing(40);
+    inputRow->addWidget(portInput);
+    inputRow->addStretch();
+
+    // Run button
+    QPushButton *startButton = new QPushButton("       Start DDOS       ");
+    startButton->setStyleSheet("background: white; color: black;");
+    startButton->setMaximumWidth(200);
+
+    // Stop button
+    QPushButton *stopButton = new QPushButton("       Stop DDOS       ");
+    stopButton->setStyleSheet("background: white; color: black;");
+    stopButton->setMaximumWidth(200);
+
+    // Put on same row
+    QHBoxLayout *buttonRow = new QHBoxLayout;
+    buttonRow->addStretch();
+    buttonRow->addWidget(startButton);
+    buttonRow->addSpacing(80);
+    buttonRow->addWidget(stopButton);
+    buttonRow->addStretch();
+
+    QObject::connect(startButton, &QPushButton::clicked, [&]() {
+        qDebug("Starting super mega DDOS on: %s", qPrintable(ipInput->text()));
+    });
+
+    QObject::connect(stopButton, &QPushButton::clicked, [&]() {
+        qDebug("Stopping super mega DDOS");
+    });
+
+    layout->addWidget(banner);
+    layout->addStretch();
+    layout->addLayout(inputRow);
+    layout->addLayout(buttonRow);
+    layout->addStretch();
+
+    window.show();
+    return app.exec();
+}
